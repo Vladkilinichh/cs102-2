42f4bae0ec7d0f0ecb292884d9020cf00b1ba8d6
Lisa <foggydisastere@mail.ru>
2018-11-03 11:24:06 +0300

    homework01 added

--- caesar.py
+++ caesar.py
@@ -1,30 +1,46 @@
 # -*- coding: utf-8 -*-
 def encrypt_caesar(plaintext):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     new_data = []
     for char in plaintext:
-        if(ord(char ) >= 97 and ord(char) <= 122): 
-            new_char = chr((ord(char) - 97 + 3) % 26 + 97)  # для маленькой
-        elif( ord(char) >= 65 and ord(char) <= 90 ):
-            new_char = chr((ord(char) - 65 + 3) % 26 + 65) # для большой
+        if 'a' <= char <= 'z':
+            new_char = (ord(char) % ord('a') + 3) % 26 + ord('a')
+        elif 'A' <= char <= 'Z':
+            new_char = (ord(char) % ord('A') + 3) % 26 + ord('A')
         else:
             new_char = char
         new_data.append(new_char)
     return "".join(new_data)
-print(encrypt_caesar(input('Введите текст на англе')))
 
 
-
-
-
-def encrypt_caesar(plaintext):
+def decrypt_caesar(plaintext):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     new_data = []
     for char in plaintext:
-        if(ord(char ) >= 97 and ord(char) <= 122): 
-            new_char = chr((ord(char) - 97 - 3) % 26 + 97)  # для маленькой
-        elif( ord(char) >= 65 and ord(char) <= 90 ):
-            new_char = chr((ord(char) - 65 - 3) % 26 + 65) # для большой
+        if 'a' <= char <= 'z':
+            new_char = (ord(char) % ord('a') - 3) % 26 - ord('a')
+        elif 'A' <= char <= 'Z':
+            new_char = (ord(char) % ord('A') - 3) % 26 - ord('A')
         else:
             new_char = char
         new_data.append(new_char)
     return "".join(new_data)
-print(encrypt_caesar(input('Введите текст на англе')))
+
